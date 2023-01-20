d8892bf19e458b211bb763afaf7fa82ef84aeb3a
klesio <klesio.dev@gmail.com>
2023-01-19 21:08:17 -0300

    install fonts inter

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello word !</Text>
+      <Text>Hello words !</Text>
       <StatusBar style="auto" />
     </View>
   );
