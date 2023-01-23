bb2a27371d547eccfb3415ef69231c4e26ca35cd
klesio <klesio.dev@gmail.com>
2023-01-23 01:32:51 -0300

    fix error in router summary

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -133,28 +133,23 @@ export async function appRoutes(app: FastifyInstance) {
     // Prisma ORM : 
 
     const summary = await prisma.$queryRaw`
-      SELECT
+     SELECT 
         D.id,
         D.date,
-          (
-            SELECT
-              cast(count(*)as float)
-            FROM day_habits DH
-            WHERE DH.day_id = D.id
-
-          )as completed,
-          (
-            SELECT
-            cast(count(*)as float)
-            FROM habit_week_days HWD
-            JOIN habit H
-              ON H.id = HWD.habit_id
-            WHERE
-              HWD.week_day = cast(strftime('%w',D.date/1000.0,'unixepoch') as int)
+        (
+          SELECT cast(count(*) as float)
+          FROM day_habits DH
+          WHERE DH.day_id = D.id
+        ) as completed,
+        (
+          SELECT cast(count(*) as float)
+          FROM habit_week_days HWD
+            JOIN habits H 
+            ON H.id = HWD.habit_id
+          WHERE HWD.week_day = cast(strftime('%w', D.date / 1000.0, 'unixepoch') as int)
             AND H.created_at <= D.date
-          )as amount
-
-        From days D
+        ) as amount
+      From days D
     `
 
 
