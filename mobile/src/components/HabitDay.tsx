b0e37af6b8d4ade570e929f62346d2102a3c3a74
klesio <klesio.dev@gmail.com>
2023-01-27 03:57:03 -0300

    added clsx in conditional in the react native

--- mobile/src/components/HabitDay.tsx
+++ mobile/src/components/HabitDay.tsx
@@ -3,10 +3,12 @@ import {
   TouchableOpacity,
   TouchableOpacityProps,
 } from 'react-native';
+import { generateProgressPercentage } from '../utils/generate-progress-percentage';
+import clsx from 'clsx';
+import dayjs from 'dayjs';
 
 const WEEK_DAY = 7;
 const SCREEN_HORIZONTAL_PADDING = (32 * 2) / 5;
-
 export const DAY_MARGIN_BETWEEN = 8;
 
 export const DAY_SIZE =
@@ -19,14 +21,37 @@ interface Touchable extends TouchableOpacityProps {
 }
 
 export function HabitDay({
-  amountOfHabits,
-  amountCompleted,
+  amountOfHabits = 0,
+  amountCompleted = 0,
   date,
   ...rest
 }: Touchable) {
+  const amountAccomplishedPercentage =
+    amountOfHabits > 0
+      ? generateProgressPercentage(amountOfHabits, amountCompleted)
+      : 0;
+
+  const today = dayjs().startOf('day').toDate();
+  const isCurrentDay = dayjs(date).isSame(today);
+
   return (
     <TouchableOpacity
-      className='bg-zinc-900 rounded-lg border-2 m-1 border-zinc-800'
+      className={clsx('rounded-lg border-2 m-1 ', {
+        ['bg-zinc-900 border-zinc-800']: amountAccomplishedPercentage === 0,
+        ['bg-violet-900 border-violet-700']:
+          amountAccomplishedPercentage > 0 && amountAccomplishedPercentage < 20,
+        ['bg-violet-800 border-violet-700']:
+          amountAccomplishedPercentage > 20 &&
+          amountAccomplishedPercentage < 40,
+        ['bg-violet-700 border-violet-600']:
+          amountAccomplishedPercentage > 40 &&
+          amountAccomplishedPercentage < 60,
+        ['bg-violet-600 border-violet-500']:
+          amountAccomplishedPercentage > 60 &&
+          amountAccomplishedPercentage < 80,
+        ['bg-violet-500 border-violet-400']: amountAccomplishedPercentage > 80,
+        ['border-white border-4']: isCurrentDay,
+      })}
       style={{ width: DAY_SIZE, height: DAY_SIZE }}
       activeOpacity={0.6}
       {...rest}
