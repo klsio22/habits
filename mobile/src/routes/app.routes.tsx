d18e52b3efca8b385d60259589e4e8192e3872bd
klesio <klesio.dev@gmail.com>
2023-01-27 04:17:41 -0300

    Get  form dates . to Register and to send a new habit

--- mobile/src/routes/app.routes.tsx
+++ mobile/src/routes/app.routes.tsx
@@ -3,7 +3,7 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 const { Navigator, Screen } = createNativeStackNavigator();
 
 import { Home } from '../screens/Home';
-import { New } from '../screens/New';
+import { New } from '../screens/NewHabit';
 import { Habit } from '../screens/Habit';
 
 export function AppRoutes() {
--- mobile/src/screens/NewHabit.tsx
+++ mobile/src/screens/NewHabit.tsx
@@ -5,11 +5,13 @@ import {
   Text,
   TextInput,
   TouchableOpacity,
+  Alert,
 } from 'react-native';
 import { BackButton } from '../components/BackButton';
 import { Checkbox } from '../components/Checkbox';
 import { Feather } from '@expo/vector-icons';
 import colors from 'tailwindcss/colors';
+import { api } from '../lib/axios';
 
 const availableWeekDays = [
   'Domingo',
@@ -23,6 +25,7 @@ const availableWeekDays = [
 ];
 
 export function New() {
+  const [title, setTitle] = useState('');
   const [weekDays, setWeekDays] = useState<number[]>([]);
 
   function handleToggleWeekDay(weekDayIndex: number) {
@@ -31,6 +34,24 @@ export function New() {
           prevState.filter((weekDay) => weekDay !== weekDayIndex)
         )
       : setWeekDays((prevState) => [...prevState, weekDayIndex]);
+  }
+
+  async function handleCreateNewHabit() {
+    try {
+      if (!title.trim() || weekDays.length === 0) {
+        Alert.alert('Novo habito', 'Informe o nome do hábito');
+      }
+
+      await api.post('/habits', { title, weekDays });
+
+      setTitle('');
+      setWeekDays([]);
+
+      
+    } catch (err) {
+      console.log('Error', err);
+      Alert.alert('Ops', 'Não foi possível criar o novo hábito');
+    }
   }
 
   return (
@@ -52,6 +73,8 @@ export function New() {
           className='h-12 pl-4 rounded-lg mt-3 bg-zinc-900 border-2 border-zinc-800 text-white focus:border-2 focus:border-green-600'
           placeholder='Exercícios, dormir bem, etc...'
           placeholderTextColor={colors.zinc[400]}
+          onChangeText={(text) => setTitle(text)}
+          value={title}
         />
 
         <Text className='font-semibold mt-4 mb-3 text-white text-base'>
@@ -70,6 +93,7 @@ export function New() {
         <TouchableOpacity
           className='w-full h-14 flex-row items-center justify-center bg-green-600 rounded-md mt-6'
           activeOpacity={0.7}
+          onPress={handleCreateNewHabit}
         >
           <Feather name='check' size={20} color={colors.white} />
 
